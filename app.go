ab4eb15e50e66940e5318ea24060e9a6a0d56f94
edwardsuwirya <edward_suwirya@yahoo.com>
2023-02-24 16:16:26 +0700

    Golang DB Transaction

---
+++ app.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type Wallet struct {
+	MembershipId int     `db:"membership_id"`
+	WalletAmount float64 `db:"wallet_amount"`
+	Description  string  `db:"description"`
+}
+
+func main() {
+	dbHost := "db-postgresql-enigma-do-user-279248-0.b.db.ondigitalocean.com"
+	dbPort := "25060"
+	dbName := "defaultdb"
+	dbUser := "doadmin"
+	dbPassword := "AVNS_V2yOmmAI1Kd9ZhBpSZJ"
+	dataSourceName := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := sqlx.Connect("postgres", dataSourceName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+	//tx := db.MustBegin()
+	//tx.NamedExec("INSERT INTO mst_membership_wallet VALUES (:membership_id, :wallet_amount, :description)", &Wallet{1, 10000, "Isi wallet"})
+	//tx.NamedExec("INSERT INTO mst_membership_wallet VALUES (:membership_id, :wallet_amount, :description)", &Wallet{2, 5000, "Isi wallet"})
+	//tx.Commit()
+
+	// Simulasi transfer
+	totalAmount := 1000.0
+	tx := db.MustBegin()
+	_, err = tx.NamedExec("update mst_membership_wallet set wallet_amount=wallet_amount+:wallet_amount,description=:description where membership_id=:membership_id", &Wallet{1, -1 * totalAmount, fmt.Sprintf("Kredit:%v", totalAmount)})
+	_, err = tx.NamedExec("update mst_membership_wallet set wallet_amount=wallet_amount+:wallet_amount,description=:description where membership_id=:membership_id", &Wallet{2, totalAmount, fmt.Sprintf("Debit:%v", totalAmount)})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module enigmacamp.com/db_trx
+
+go 1.18
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+)
